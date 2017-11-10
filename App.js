edd614bcdb006e4de45aae7d6ab17ddf2ea03358
TaeJinKwon <tjkwon@barogo.com>
2017-11-10 18:22:29 +0900

    Apply Redux

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Timer from './components/Timer';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import Timer from "./components/Timer";
+import reducer from "./reducer";
+
+let store = createStore(reducer);
+
+console.log(store.getState());
 
 export default class App extends React.Component {
   render() {
     return (
-      <Timer/>
+      <Provider store={store}>
+        <Timer />
+      </Provider>
     );
   }
 }
@@ -13,8 +22,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
--- components/Timer/index.js
+++ components/Timer/index.js
@@ -1,45 +1,13 @@
-import React from "react";
-import {View, Text, StyleSheet, StatusBar, Alert} from "react-native";
-import Button from "../Button";
+import { connect } from "react-redux";
+import Timer from "./presenter";
 
-class Timer extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-            <StatusBar barStyle={"light-content"}/>
-                <View style={styles.upper}>
-                    <Text style={styles.timer}>25:00</Text>
-                </View>
-                <View style={styles.lower}>
-                    <Button iconName="play-circle-o" onPress={() => Alert.alert("start Press work")}/>
-                    <Button iconName="stop-circle-o" onPress={() => Alert.alert("stop press work")}/>
-                </View>
-            </View>
-
-        );
-    }
+function mapStateToProps(state) {
+    const { isPlaying, elapsedTime, timerDuration} = state;
+    return {
+        isPlaying,
+        elapsedTime,
+        timerDuration
+    };
 }
 
-const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: "red"
-    },
-    upper:{
-        flex: 1,
-        justifyContent:"center",
-        alignItems: "center"    
-    },
-    lower:{
-        flex: 1,
-        justifyContent:"center",
-        alignItems: "center"
-    },
-    timer: {
-        color:"white",
-        fontSize:120,
-        fontWeight:"100"
-    }
-});
-
-export default Timer;
+export default connect(mapStateToProps)(Timer);
