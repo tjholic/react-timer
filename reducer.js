8ff1eee86758f01724b7ed4a5060c43e2cd081df
TaeJinKwon <tjkwon@barogo.com>
2017-11-13 18:30:30 +0900

    Connecting Action

---
+++ reducer.js
@@ -0,0 +1,105 @@
+// Imports
+
+// TODO: Actions
+
+const START_TIMER = "START_TIMER";
+const RESTART_TIMER = "RESTART_TIMER";
+const ADD_SECOND = "ADD_SECOND";
+
+// Action Creators
+
+function startTimer() {
+  return {
+    type: START_TIMER
+  };
+}
+
+function restartTimer() {
+  return {
+    type: RESTART_TIMER
+  };
+}
+
+function addSecond() {
+  return {
+    type: ADD_SECOND
+  };
+}
+
+// Reducer
+
+const TIMER_DURATION = 1500;
+
+const initialState = {
+  isPlaying: false,
+  elapsedTime: 0,
+  timerDuration: TIMER_DURATION
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case START_TIMER: {
+      console.log("start_timer!!!");
+      return applyStartTimer(state, action);
+    }
+    case RESTART_TIMER: {
+      console.log("restart_timer!!!");
+      return applyRestartTimer(state, action);
+    }
+    case ADD_SECOND: {
+      console.log("add_second");
+      return applyAddSecond(state, action);
+    }
+    default: {
+      console.log("default");
+      return state;
+    }
+  }
+}
+
+// Reducer Functions
+
+function applyStartTimer(state, action) {
+  return {
+    ...state,
+    isPlaying: true,
+    elapsedTime: 0
+  };
+}
+
+function applyRestartTimer(state, action) {
+  return {
+    ...state,
+    isPlaying: false,
+    elapsedTime: 0
+  };
+}
+
+function applyAddSecond(state, action) {
+  const { elapsedTime } = state;
+  if (elapsedTime < TIMER_DURATION) {
+    return {
+      ...state,
+      elapsedTime: elapsedTime + 1
+    };
+  } else {
+    return {
+      ...state,
+      isPlaying: false
+    };
+  }
+}
+
+// Exports
+
+const actionCreators = {
+  startTimer,
+  restartTimer,
+  addSecond
+};
+
+export { actionCreators };
+
+// Default
+
+export default reducer;
